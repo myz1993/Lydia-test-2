a14186a63c0430c80fd1e3ff18703e00d31e6742
Leo Liu <leo.liu@vudoo.com>
2020-09-26 19:58:59 +1000

    webpack run success

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'production',
   entry: {
@@ -17,4 +18,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
 };
