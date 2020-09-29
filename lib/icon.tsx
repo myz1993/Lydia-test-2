ca0801fe133dc970d1ed2f3a97fcc3d9b8c2b20e
Leo Liu <leo.liu@vudoo.com>
2020-09-29 22:28:13 +1000

    feature: automaticlly add icon

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
-
+// import '../Icons/wechat.svg';
+// import '../Icons/alipay.svg';
+// import '../Icons/qq.svg';
+import './importicons';
 interface IconProps {
   name: string;
 }
 const Icon: React.FunctionComponent<IconProps> = (props) => {
-  return <span>{props.name}</span>;
+  return (
+    <span>
+      <svg>
+        <use xlinkHref={`#${props.name}`}></use>
+      </svg>
+    </span>
+  );
 };
 export default Icon;
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Icon from './icon';
+//import wechat from '../Icons/wechat.svg';
 
 ReactDOM.render(
   <div>
     <Icon name="wechat" />
+    <Icon name="alipay" />
+    <Icon name="qq" />
   </div>,
-  document.body
+  document.querySelector('#Root')
 );
