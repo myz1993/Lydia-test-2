4f0ddb96a1fad202a65834bbc25e41e3137859ea
Leo Liu <leo.liu@vudoo.com>
2020-09-29 15:25:38 +1000

    feature: create Icon component

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from './button';
+import Icon from './icon';
 
-ReactDOM.render(<Button />, document.body);
+ReactDOM.render(
+  <div>
+    <Icon name="wechat" />
+  </div>,
+  document.body
+);
