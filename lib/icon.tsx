78cdbf3d68805da4227469c8f540e8b692c2b5ea
Leo Liu <leo.liu@vudoo.com>
2020-10-01 13:16:08 +1000

    feature: add onClick event

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-// import '../Icons/wechat.svg';
-// import '../Icons/alipay.svg';
-// import '../Icons/qq.svg';
-import './importicons';
+import '../Icons/wechat.svg';
+import '../Icons/alipay.svg';
+import '../Icons/qq.svg';
+//import './importicons';
+import './icon.scss';
 interface IconProps {
   name: string;
+  onClick: () => void;
 }
 const Icon: React.FunctionComponent<IconProps> = (props) => {
   return (
-    <span>
-      <svg>
-        <use xlinkHref={`#${props.name}`}></use>
-      </svg>
-    </span>
+    <svg className="lui-icon" onClick={props.onClick}>
+      <use xlinkHref={`#${props.name}`}></use>
+    </svg>
   );
 };
 export default Icon;
--- lib/index.tsx
+++ lib/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import Icon from './icon';
 //import wechat from '../Icons/wechat.svg';
 
+const fn = () => {
+  console.log('fn');
+};
 ReactDOM.render(
   <div>
-    <Icon name="wechat" />
-    <Icon name="alipay" />
-    <Icon name="qq" />
+    {/* <Icon name="wechat" />
+    <Icon name="alipay" /> */}
+    <Icon name="qq" onClick={fn} />
   </div>,
   document.querySelector('#Root')
 );
